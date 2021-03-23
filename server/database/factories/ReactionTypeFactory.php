4b9b01f11fb50a806e685c4fe338b1131251c59d
baretabel <baretabel4@gmail.com>
2021-03-23 12:05:11 +0400

    doc structure

---
+++ server/database/factories/ReactionTypeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ReactionType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReactionTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ReactionType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
