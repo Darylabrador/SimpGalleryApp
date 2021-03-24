4a90ad8021a5508e9e4fb0f2e4bc21f1dedbbd0b
baretabel <baretabel4@gmail.com>
2021-03-24 10:24:51 +0400

    cover edit

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -12,24 +12,26 @@ use Illuminate\Support\Str;
 
 class AlbumController extends Controller
 {
-    /**
-     * Create album with cover image.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(Request $request){
-        $validator = Validator::make(
-            $request->all(),
-            [   
-                'label' => 'required',
-                'cover' => 'required|file|mimes:jpg,jpeg,png|max:5000',
-            ],
-            [
-                'file'  => 'Image non fournis',
-                'mimes' => 'Extension invalide',
-                'max'   => '5Mb maximum'
-            ]
-        );
+public function create(Request $request){
+    $validator = Validator::make(
+        $request->all(),
+        [   
+            'label' => 'required',
+            'cover' => 'required|file|mimes:jpg,jpeg,png|max:5000',
+        ],
+        [
+            'file'  => 'Image non fournis',
+            'mimes' => 'Extension invalide',
+            'max'   => '5Mb maximum'
+        ]
+    );
+    $errors = $validator->errors();
+        if (count($errors) != 0) {
+            return response()->json([
+                'success' => false,
+                'message' => $errors->first()
+            ]);
+        }
 
         $loggedUser = Auth::user();
         $userId = $loggedUser->id;
@@ -45,7 +47,55 @@ class AlbumController extends Controller
 
         return response()->json([
             'success' => true,
-            'message' => "Mise à jour effectuée"
+            'message' => "Album créé"
+        ]);
+}
+/**
+*Edit cover
+*Illuminate\Http\Request
+*/
+public function editCover(Request $request,$albumId) {
+    $validator = Validator::make(
+        $request->all(),
+        [
+            'cover' => 'required|file|mimes:jpg,jpeg,png|max:5000',
+        ],
+        [
+            'file'  => 'Image non fournis',
+            'mimes' => 'Extension invalide',
+            'max'   => '5Mb maximum'
+        ]
+    );
+
+    $errors = $validator->errors();
+    if (count($errors) != 0) {
+        return response()->json([
+            'success' => false,
+            'message' => $errors->first()
         ]);
     }
+
+    
+
+    $album     = Album::whereId($albumId)->first();
+    $oldImage = $album->cover;
+    
+    
+        $oldFilePath = public_path('images') . '/' . $oldImage;
+        unlink($oldFilePath);
+    
+
+    $cover    = $validator->validated()['cover'];
+    $extension      = $cover->getClientOriginalExtension();
+    $image          = time() . rand() . '.' . $extension;
+    $cover->move(public_path('images/profils'), $image);
+    $album->cover = $image;
+    $album->save();
+
+    return response()->json([
+        'success' => true,
+        'message' => "Mise à jour effectuée"
+    ]);
+}
+  
 }
--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Validator;
 
 class PhotoController extends Controller
 {
+    /**
+    *Add pic to an album
+    *Illuminate\Http\Request
+    */
     public function create(Request $request, $albumId){
         $validator = Validator::make(
             $request->all(),
@@ -40,17 +44,19 @@ class PhotoController extends Controller
                 $album->album_id    = $albumId;
                 $album->label       = $image;
                 $album->save();
+
+                
             }
             
             return response()->json([
                 'success' => true,
-                'message' => "Mise à jour effectuée"
+                'message' => "Photo ajoutée"
             ]);
         }
 
         return response()->json([
             'success' => true,
-            'message' => "Mise à jour effectuée"
+            'message' => "Photo(s) ajoutée(s)"
         ]);
     }
 }
