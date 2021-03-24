2b8879315768c167b05ecaab2edd7967dbb1af6f
Darylabrador <abrador.daryl@gmail.com>
2021-03-24 10:21:41 +0400

    Rework : photoController -- create function

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -10,34 +10,47 @@ use Illuminate\Support\Facades\Validator;
 class PhotoController extends Controller
 {
     public function create(Request $request, $albumId){
-        
-            $validator = Validator::make(
-                $request->all(),
-                [   
-                    'images.*' => 'required|file|mimes:jpg,jpeg,png|max:5000',
-                ],
-                [
-                    'file'  => 'Image non fournis',
-                    'mimes' => 'Extension invalide',
-                    'max'   => '5Mb maximum'
-                ]
-            );
+        $validator = Validator::make(
+            $request->all(),
+            [   
+                'images'   => "required",
+                'images.*' => 'file|mimes:jpg,jpeg,png|max:5000',
+            ],
+            [
+                'file'  => 'Image non fournis',
+                'mimes' => 'Extension invalide',
+                'max'   => '5Mb maximum'
+            ]
+        );
 
-            foreach ($validator->validated()['images'] as $key => $pic) {
-                
-                $extension          = $pic->getClientOriginalExtension();
+        $errors = $validator->errors();
+        if (count($errors) != 0) {
+            return response()->json([
+                'success' => false,
+                'message' => $errors->first()
+            ]);
+        }
+
+        if($request->hasFile('images')){
+            foreach ($request->file('images') as $file) {
+                $extension          = $file->getClientOriginalExtension();
                 $image              = time() . rand() . '.' . $extension;
-                $pic->move(public_path('img/albums'), $image);
+                $file->move(public_path('img/albums'), $image);
                 $album              = new Album;
                 $album->album_id    = $albumId;
                 $album->label       = $image;
                 $album->save();
-
-                return response()->json([
-                    'success' => true,
-                    'message' => "Mise à jour effectuée"
-                ]);
             }
-        
+            
+            return response()->json([
+                'success' => true,
+                'message' => "Mise à jour effectuée"
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => "Mise à jour effectuée"
+        ]);
     }
 }
