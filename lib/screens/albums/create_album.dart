0a91ac9deb8ec8054fe14b7af767b20cde88e0b9
SharlyneEvynna <56205156+fredy-ID@users.noreply.github.com>
2021-04-21 14:34:14 +0400

    maj create album

--- lib/screens/albums/create_album.dart
+++ lib/screens/albums/create_album.dart
@@ -28,65 +28,91 @@ class _CreateAlbumState extends State<CreateAlbum> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 30.0),
           child: Form(
             key: _formKey,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Image.asset('assets/sample-01.jpg',
-                    height: 100.0, width: 100.0),
+                Container(
+                    height: 300,
+                    width: 500,
+                    child: Stack(children: <Widget>[
+                      Image.asset('assets/sample-01.jpg', fit: BoxFit.cover),
+                      Center(
+                        child: OutlinedButton(
+                          style: ButtonStyle(
+                            foregroundColor: MaterialStateProperty.all<Color>(
+                                Colors.lightBlue),
+                          ),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/home');
+                          },
+                          child: Text("Selectionner une image"),
+                        ),
+                      ),
+                    ])),
+                SizedBox(height: 50.0),
                 Center(
                   child: Text('Ajouter un album',
                       style: Theme.of(context).textTheme.headline6),
                 ),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Label', border: OutlineInputBorder()),
-                  validator: (val) =>
-                      val!.isEmpty ? 'Entrez un nom pour votre album' : null,
-                  onChanged: (val) => label = val,
-                ),
-                SizedBox(height: 10.0),
-                ElevatedButton(
-                  onPressed: () async {
-                    // if (_formKey.currentState!.validate()) {
-                    //   url = Uri.parse(
-                    //       // "${DotEnv.env['DATABASE_URL']}/api/connexion");
-                    //       "http://78c7949cddff.ngrok.io/api/inscription");
-                    //   response = await http.post(url, body: {
-                    //     'identifiant': email,
-                    //     'password': password,
-                    //   }, headers: {
-                    //     "Accept": "application/json"
-                    //   });
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 50.0, horizontal: 30.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      SizedBox(height: 30.0),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'Nom de l\'album',
+                            border: OutlineInputBorder()),
+                        validator: (val) => val!.isEmpty
+                            ? 'Entrez un nom pour votre album'
+                            : null,
+                        onChanged: (val) => label = val,
+                      ),
+                      SizedBox(height: 10.0),
+                      ElevatedButton(
+                        onPressed: () async {
+                          // if (_formKey.currentState!.validate()) {
+                          //   url = Uri.parse(
+                          //       // "${DotEnv.env['DATABASE_URL']}/api/connexion");
+                          //       "http://78c7949cddff.ngrok.io/api/inscription");
+                          //   response = await http.post(url, body: {
+                          //     'identifiant': email,
+                          //     'password': password,
+                          //   }, headers: {
+                          //     "Accept": "application/json"
+                          //   });
 
-                    //   if (response.statusCode == 200) {
-                    //     storage.setItem("SimpGalleryToken", response.body);
-                    //     Navigator.pushNamed(context, '/home');
-                    //   } else {
-                    //     showToast(
-                    //       "Une erreur est survenue",
-                    //       context: context,
-                    //       animation: StyledToastAnimation.scale,
-                    //       reverseAnimation: StyledToastAnimation.fade,
-                    //       position: StyledToastPosition.bottom,
-                    //       animDuration: Duration(seconds: 1),
-                    //       duration: Duration(seconds: 4),
-                    //       curve: Curves.elasticOut,
-                    //       reverseCurve: Curves.linear,
-                    //     );
-                    //   }
-                    // }
-                  },
-                  child: Text('Créer'),
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/home');
-                  },
-                  child: Text("Annuler"),
+                          //   if (response.statusCode == 200) {
+                          //     storage.setItem("SimpGalleryToken", response.body);
+                          //     Navigator.pushNamed(context, '/home');
+                          //   } else {
+                          //     showToast(
+                          //       "Une erreur est survenue",
+                          //       context: context,
+                          //       animation: StyledToastAnimation.scale,
+                          //       reverseAnimation: StyledToastAnimation.fade,
+                          //       position: StyledToastPosition.bottom,
+                          //       animDuration: Duration(seconds: 1),
+                          //       duration: Duration(seconds: 4),
+                          //       curve: Curves.elasticOut,
+                          //       reverseCurve: Curves.linear,
+                          //     );
+                          //   }
+                          // }
+                        },
+                        child: Text('Ajouter'),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/home');
+                        },
+                        child: Text("Annuler"),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
