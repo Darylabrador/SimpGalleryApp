2fbd5ebb0355ccc51e3b6b55d9578007e6c6439e
Darylabrador <abrador.daryl@gmail.com>
2021-06-01 13:08:08 +0400

    trash functionnalities

--- lib/screens/albums/albumTrash.dart
+++ lib/screens/albums/albumTrash.dart
@@ -39,9 +39,172 @@ class _AlbumTrash extends State<AlbumTrash> {
         _albumTrashList = parsedJson['data'];
       });
 
-      
       print(_albumTrashList);
     }
+  }
+
+  Future restoreDialog(albumId) {
+    var token = storage.getItem('SimpGalleryToken');
+    return showDialog<String>(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  'Voulez-vous vraiment restaurer cet album ?',
+                  style: TextStyle(color: Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          OutlinedButton(
+            child: Text(
+              'Annuler',
+              style: TextStyle(color: Colors.black38),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          OutlinedButton(
+            child: Text(
+              'Valider',
+              style: TextStyle(color: Colors.redAccent),
+            ),
+            onPressed: () async {
+              var url = Uri.parse(
+                  "${DotEnv.env['DATABASE_URL']}/api/album/$albumId/trash/restore");
+              var response = await http.put(url, body: null, headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + token
+              });
+              if (response.statusCode == 200) {
+                var parsedJson = json.decode(response.body);
+                showToast(
+                  parsedJson['message'],
+                  context: context,
+                  animation: StyledToastAnimation.scale,
+                  reverseAnimation: StyledToastAnimation.fade,
+                  position: StyledToastPosition.bottom,
+                  animDuration: Duration(seconds: 1),
+                  duration: Duration(seconds: 4),
+                  curve: Curves.elasticOut,
+                  reverseCurve: Curves.linear,
+                );
+
+                if (parsedJson['success']) {
+                  Navigator.of(context)
+                    ..pop()
+                    ..pop()
+                    ..pushNamed('/home');
+                }
+              } else {
+                showToast(
+                  "Une erreur est survenue",
+                  context: context,
+                  animation: StyledToastAnimation.scale,
+                  reverseAnimation: StyledToastAnimation.fade,
+                  position: StyledToastPosition.bottom,
+                  animDuration: Duration(seconds: 1),
+                  duration: Duration(seconds: 4),
+                  curve: Curves.elasticOut,
+                  reverseCurve: Curves.linear,
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future confirmDeleteDialog(albumId) {
+    var token = storage.getItem('SimpGalleryToken');
+    return showDialog<String>(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  'Voulez-vous vraiment supprimer cet album ?',
+                  style: TextStyle(color: Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          OutlinedButton(
+            child: Text(
+              'Annuler',
+              style: TextStyle(color: Colors.black38),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          OutlinedButton(
+            child: Text(
+              'Valider',
+              style: TextStyle(color: Colors.redAccent),
+            ),
+            onPressed: () async {
+              var url = Uri.parse(
+                  "${DotEnv.env['DATABASE_URL']}/api/album/$albumId/delete/confirm");
+              var response = await http.delete(url, headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + token
+              });
+              if (response.statusCode == 200) {
+                var parsedJson = json.decode(response.body);
+                showToast(
+                  parsedJson['message'],
+                  context: context,
+                  animation: StyledToastAnimation.scale,
+                  reverseAnimation: StyledToastAnimation.fade,
+                  position: StyledToastPosition.bottom,
+                  animDuration: Duration(seconds: 1),
+                  duration: Duration(seconds: 4),
+                  curve: Curves.elasticOut,
+                  reverseCurve: Curves.linear,
+                );
+
+                if (parsedJson['success']) {
+                  Navigator.of(context)
+                    ..pop()
+                    ..pop()
+                    ..pushNamed('/home');
+                }
+              } else {
+                showToast(
+                  "Une erreur est survenue",
+                  context: context,
+                  animation: StyledToastAnimation.scale,
+                  reverseAnimation: StyledToastAnimation.fade,
+                  position: StyledToastPosition.bottom,
+                  animDuration: Duration(seconds: 1),
+                  duration: Duration(seconds: 4),
+                  curve: Curves.elasticOut,
+                  reverseCurve: Curves.linear,
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -77,7 +240,43 @@ class _AlbumTrash extends State<AlbumTrash> {
           ),
         ],
       ),
-      body: null,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(bottom: 80.0),
+          height: 500,
+          child: GridView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.all(8),
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemCount: _albumTrashList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onLongPress: () {
+                  confirmDeleteDialog(_albumTrashList[index]['id'].toString());
+                },
+                onDoubleTap: () {
+                  restoreDialog(_albumTrashList[index]['id'].toString());
+                },
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.network(
+                          "${DotEnv.env['DATABASE_URL']}/img/" +
+                              _albumTrashList[index]['cover'],
+                          height: 110,
+                          width: 200,
+                          fit: BoxFit.cover),
+                      Text(_albumTrashList[index]['label'].toString())
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/albums/photoTrash.dart
+++ lib/screens/albums/photoTrash.dart
@@ -30,7 +30,8 @@ class _PhotoTrash extends State<PhotoTrash> {
     final LocalStorage storage = new LocalStorage('sharePhoto');
     var token = storage.getItem('SimpGalleryToken');
     var id = widget.argsData["arrayData"]["id"];
-    var url = Uri.parse("${DotEnv.env['DATABASE_URL']}/api/photo/album/$id/trash/list");
+    var url = Uri.parse(
+        "${DotEnv.env['DATABASE_URL']}/api/photo/album/$id/trash/list");
     var getAlbums = await http.get(url, headers: {
       "Accept": "application/json",
       "Authorization": "Bearer " + token
@@ -44,6 +45,188 @@ class _PhotoTrash extends State<PhotoTrash> {
 
       print(_photoTrashList);
     }
+  }
+
+  Future restoreDialog(photoId) {
+    var token = storage.getItem('SimpGalleryToken');
+    return showDialog<String>(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  'Voulez-vous vraiment restaurer cette photo ?',
+                  style: TextStyle(color: Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          OutlinedButton(
+            child: Text(
+              'Annuler',
+              style: TextStyle(color: Colors.black38),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          OutlinedButton(
+            child: Text(
+              'Valider',
+              style: TextStyle(color: Colors.redAccent),
+            ),
+            onPressed: () async {
+              var url = Uri.parse(
+                  "${DotEnv.env['DATABASE_URL']}/api/photo/$photoId/trash/restore");
+              var response = await http.put(url, body: null, headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + token
+              });
+              if (response.statusCode == 200) {
+                var parsedJson = json.decode(response.body);
+                showToast(
+                  parsedJson['message'],
+                  context: context,
+                  animation: StyledToastAnimation.scale,
+                  reverseAnimation: StyledToastAnimation.fade,
+                  position: StyledToastPosition.bottom,
+                  animDuration: Duration(seconds: 1),
+                  duration: Duration(seconds: 4),
+                  curve: Curves.elasticOut,
+                  reverseCurve: Curves.linear,
+                );
+
+                if (parsedJson['success']) {
+                  if (widget.argsData["isShare"]) {
+                    Navigator.of(context)
+                      ..pop()
+                      ..pop()
+                      ..pushNamed('/shared',
+                          arguments: widget.argsData["arrayData"]);
+                  } else {
+                    Navigator.of(context)
+                      ..pop()
+                      ..pop()
+                      ..pushNamed('/photos',
+                          arguments: widget.argsData["arrayData"]);
+                  }
+                }
+              } else {
+                showToast(
+                  "Une erreur est survenue",
+                  context: context,
+                  animation: StyledToastAnimation.scale,
+                  reverseAnimation: StyledToastAnimation.fade,
+                  position: StyledToastPosition.bottom,
+                  animDuration: Duration(seconds: 1),
+                  duration: Duration(seconds: 4),
+                  curve: Curves.elasticOut,
+                  reverseCurve: Curves.linear,
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future confirmDeleteDialog(photoId) {
+    var token = storage.getItem('SimpGalleryToken');
+    return showDialog<String>(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  'Voulez-vous vraiment supprimer cette photo ?',
+                  style: TextStyle(color: Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          OutlinedButton(
+            child: Text(
+              'Annuler',
+              style: TextStyle(color: Colors.black38),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          OutlinedButton(
+            child: Text(
+              'Valider',
+              style: TextStyle(color: Colors.redAccent),
+            ),
+            onPressed: () async {
+              var url = Uri.parse(
+                  "${DotEnv.env['DATABASE_URL']}/api/photo/$photoId/delete/confirm");
+              var response = await http.delete(url, headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + token
+              });
+              if (response.statusCode == 200) {
+                var parsedJson = json.decode(response.body);
+                showToast(
+                  parsedJson['message'],
+                  context: context,
+                  animation: StyledToastAnimation.scale,
+                  reverseAnimation: StyledToastAnimation.fade,
+                  position: StyledToastPosition.bottom,
+                  animDuration: Duration(seconds: 1),
+                  duration: Duration(seconds: 4),
+                  curve: Curves.elasticOut,
+                  reverseCurve: Curves.linear,
+                );
+
+                if (parsedJson['success']) {
+                  if (widget.argsData["isShare"]) {
+                    Navigator.of(context)
+                      ..pop()
+                      ..pop()
+                      ..pushNamed('/shared',
+                          arguments: widget.argsData["arrayData"]);
+                  } else {
+                    Navigator.of(context)
+                      ..pop()
+                      ..pop()
+                      ..pushNamed('/photos',
+                          arguments: widget.argsData["arrayData"]);
+                  }
+                }
+              } else {
+                showToast(
+                  "Une erreur est survenue",
+                  context: context,
+                  animation: StyledToastAnimation.scale,
+                  reverseAnimation: StyledToastAnimation.fade,
+                  position: StyledToastPosition.bottom,
+                  animDuration: Duration(seconds: 1),
+                  duration: Duration(seconds: 4),
+                  curve: Curves.elasticOut,
+                  reverseCurve: Curves.linear,
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -93,7 +276,42 @@ class _PhotoTrash extends State<PhotoTrash> {
           ),
         ],
       ),
-      body: null,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(bottom: 80.0),
+          height: 500,
+          child: GridView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.all(8),
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemCount: _photoTrashList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onLongPress: () {
+                  confirmDeleteDialog(_photoTrashList[index]['id'].toString());
+                },
+                onDoubleTap: () {
+                  restoreDialog(_photoTrashList[index]['id'].toString());
+                },
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.network(
+                          "${DotEnv.env['DATABASE_URL']}/img/" +
+                              _photoTrashList[index]['label'],
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.cover),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
